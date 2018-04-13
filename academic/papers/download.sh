f598576aa13ae19ac923abd59b18a302b0b81bb6
Ed Powley <edpowley@gmail.com>
2018-04-13 15:29:58 +0100

    Host all papers locally

---
+++ academic/papers/download.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+wget http://metamakersinstitute.com/home/wp-content/uploads/2016/10/FluidicGames_ICCC17.pdf
+wget http://metamakersinstitute.com/home/wp-content/uploads/2016/10/Gamika_MICI17.pdf
+wget http://metamakersinstitute.com/home/wp-content/uploads/2016/10/GameDemocratisation_CROP17.pdf
+wget http://metamakersinstitute.com/home/wp-content/uploads/2016/10/wevva_aiide17.pdf
+wget http://metamakersinstitute.com/home/wp-content/uploads/2016/10/Wevva_AISB17.pdf
+wget http://metamakersinstitute.com/home/wp-content/uploads/2016/10/powley_aiide17.pdf
+wget http://nemog.org/sites/default/files/TCIAIG-Player%20Preference%20and%20Style%20in%20a%20Leading%20Mobile%20Card%20Game.pdf
+wget http://www-users.cs.york.ac.uk/~nsephton/papers/cig2014-low-heuristics.pdf
+wget http://www-users.cs.york.ac.uk/~nsephton/papers/wcci2014-ismcts-parallelization.pdf
+wget http://www.diego-perez.net/papers/MOPTSPComp_CEC2014.pdf
+wget http://www-users.cs.york.ac.uk/susan/bib/ss/nonstd/jca11.pdf
+wget http://uncomp.uwe.ac.uk/automata2008/files/quadprize_powley.pdf
+wget http://www-users.cs.york.ac.uk/susan/bib/ss/nonstd/jca09.pdf
+wget http://www-users.cs.york.ac.uk/susan/bib/ss/nonstd/jca08.pdf
+wget http://www-users.cs.york.ac.uk/susan/bib/ss/nonstd/auto08.pdf
